c6b1aa8c1418f31c05b02260bcc21a5ad5cd00a5
bhavanigit <bhavanirajendran90@gmail.com>
2022-10-26 22:53:44 +0530

    testone

--- src/org/gittest/EmployeeDetails.java
+++ src/org/gittest/EmployeeDetails.java
@@ -2,4 +2,20 @@ package org.gittest;
 
 public class EmployeeDetails {
 
+private void empName() {
+  System.out.println("sajan"); 
+  }
+private void empNumber() {
+	System.out.println("9876543210");
+
 }
+
+public static void main(String[] args) {
+EmployeeDetails e = new EmployeeDetails();	
+e.empName();
+e.empNumber();
+}
+
+
+}
+
